871030a1d27a29b45527c0d3e8ab907fc1110fd9
dflaven <denis.flaven@combodo.com>
2012-11-02 11:51:57 +0100

    Update src/convertSyntax.php
    - "some" support of tables
    - fixed the conversion of 'code' blocks and ''images'

--- src/convertSyntax.php
+++ src/convertSyntax.php
@@ -19,6 +19,8 @@
  *
  * @author Andrei Nicholson
  * @since  2012-05-07
+ * 
+ * @author Denis Flaven
  */
 
 /**
@@ -27,6 +29,10 @@
  * Regular expressions originally by Johannes Buchner
  * <buchner.johannes [at] gmx.at>.
  *
+ * Changes by Denis Flaven
+ * - "some" support of tables
+ * - fixed the conversion of 'code' blocks and ''images'
+ * 
  * Changes by Frederik Tilkin:
  *
  * <ul>
@@ -36,6 +42,7 @@
  * <li>added functionality (multiple lines starting with a space become CODE
  *     blocks)</li>
  * </ul>
+ * 
  */
 class mediaWikiConverter {
     /** Original MediaWiki record. */
@@ -65,7 +72,8 @@ class mediaWikiConverter {
      * @author Frederik Tilkin
      */
     public function convert() {
-        $record = $this->convertCodeBlocks($this->record);
+        $record = $this->removeFakeURLs($this->record);
+        $record = $this->convertCodeBlocks($record);
         $record = $this->convertHeadings($record);
         $record = $this->convertList($record);
         $record = $this->convertUrlText($record);
@@ -73,6 +81,7 @@ class mediaWikiConverter {
         $record = $this->convertBoldItalic($record);
         $record = $this->convertTalks($record);
         $record = $this->convertImagesFiles($record);
+        $record = $this->convertTables($record);
 
         if (count($this->codeBlock) > 0) {
             $record = $this->replaceStoredCodeBlocks($record);
@@ -82,6 +91,21 @@ class mediaWikiConverter {
     }
 
     /**
+     * Fake URLs like http(s)://<your_web_server>/your_app/ seems to break DocuWiki's parser. Escape them.
+     *
+     * @param string $record
+     *
+     * @return string
+     */
+    private function removeFakeURLs($record) {
+        $patterns = array(
+                          '@http\(s\)://([^ ]+)@' => '<nowiki>http(s)://${1}</nowiki>');
+
+        return preg_replace(array_keys($patterns), array_values($patterns),
+                            $record);
+    }
+    
+  	/**
      * Code blocks.
      *
      * @param string $record
@@ -91,7 +115,7 @@ class mediaWikiConverter {
     private function convertCodeBlocks($record) {
         $patterns = array(
                           // Also replace ALL //... strings
-                          '/([^\/]*)(\/\/+)/' => '\1<nowiki>\2<\/nowiki>',
+                          //'/([^\/]*)(\/\/+)/' => '\1<nowiki>\2<\/nowiki>', // DF: removed since in my case it was causing more harm than good
 
                           // Change the ones that have been replaced in a link
                           // [] BACK to normal (do it twice in case
@@ -100,7 +124,8 @@ class mediaWikiConverter {
                           '/([\[][^\[]*)(<nowiki>)(\/\/+)(<\/nowiki>)([^\]]*)/' => '\1\3\5',
                           '/([\[][^\[]*)(<nowiki>)(\/\/+)(<\/nowiki>)([^\]]*)/' => '\1\3\5',
 
-                          '@<pre>(.*?)?</pre>@es'     => '$this->storeCodeBlock(\'\1\')',
+                          '@<tt>(.*?)?</tt>@es'     => '$this->storeCodeBlock(\'\1\')',
+        				  '@<pre>(.*?)?</pre>@es'     => '$this->storeCodeBlock(\'\1\')',
                           '@</code>\n[ \t]*\n<code>@' => '');
 
         return preg_replace(array_keys($patterns), array_values($patterns),
@@ -118,9 +143,8 @@ class mediaWikiConverter {
      */
     private function storeCodeBlock($code) {
         $this->codeBlock[] = $code;
-
         $replace = $this->placeholder . (count($this->codeBlock) - 1) . '@@';
-
+        
         return "<code>$replace</code>";
     }
 
@@ -132,6 +156,7 @@ class mediaWikiConverter {
      * @return string Record with placeholders removed.
      */
     private function replaceStoredCodeBlocks($record) {
+    	
         for ($i = 0, $numBlocks = count($this->codeBlock); $i < $numBlocks; $i++) {
             $record = str_replace($this->placeholder . $i . '@@',
                                   $this->codeBlock[$i],
@@ -148,7 +173,8 @@ class mediaWikiConverter {
      * @return string
      */
     private function convertImagesFiles($record) {
-        $patterns = array('/\[\[([media]|[medium]|[bild]|[image]|[datei]|[file]):([^\|\S]*)\|?\S*\]\]/i' => '{{mediawiki:\2}}');
+        // $patterns = array('/\[\[([media]|[medium]|[bild]|[image]|[datei]|[file]):([^\|\S]*)\|?\S*\]\]/i' => '{{:mediawiki:\2}}');
+    	$patterns = array('/\[\[(file|image|media|medium|bild|datei):([^\|\]]*)\|?([^\]]*)\]\]/i' => '{{:mediawiki:\2?\3}}');
 
         return preg_replace(array_keys($patterns), array_values($patterns),
                             $record);
@@ -235,9 +261,10 @@ class mediaWikiConverter {
                           '/^\*{3} /m' => '      * ',
                           '/^\*{4} /m' => '        * ',
                           '/^# /m'     => '  - ',
-                          '/^#{2} /m'  => '    - ',
-                          '/^#{3} /m'  => '      - ',
-                          '/^#{4} /m'  => '        - ');
+        				  '/^#([^#])/m'     => '  - ${1}',
+                          '/^## /m'  => '    - ',
+                          '/^#### /m'  => '      - ',
+                          '/^##### /m'  => '        - ');
 
         return preg_replace(array_keys($patterns), array_values($patterns),
                             $record);
@@ -259,14 +286,15 @@ class mediaWikiConverter {
                           '/^=====(.+)=====\s*$/m'   => '==\1==',
                           '/^====(.+)====\s*$/m'     => '===\1===',
                           '/^===(.+)===\s*$/m'       => '====\1====',
-                          '/^==(.+)==\s*$/m'         => '=====\1=====');
-
+                          '/^==(.+)==\s*$/m'         => '=====\1=====',
+        				  '/^=(.+)=\s*$/m'         => '=====\1=====');
+        
         // Insert a unique string to the replacement so that it won't be
         // caught in a search later.
         // @todo A lambda function can be used when PHP 5.4 is required.
         array_walk($patterns,
                    create_function('&$v, $k',
-                                   '$v = "' . $this->placeholder . '" . $v;'));
+                                   '$v = "' . $this->placeholder.'Heading'. '" . $v;')); //Added 'Heading' since the same "unique" marker was used for code blocks !!
 
         $convertedRecord = preg_replace(array_keys($patterns),
                                         array_values($patterns), $record);
@@ -277,6 +305,85 @@ class mediaWikiConverter {
         }
 
         // Strip out the unique strings.
-        return str_replace($this->placeholder, '', $convertedRecord);
+        return str_replace($this->placeholder.'Heading', '', $convertedRecord); //Added 'Heading' since the same "unique" marker was used for code blocks !!
+    }
+    /**
+     * Convert tables. Syntax between MediaWiki and DokuWiki are quite different
+     * Tables are surrounded by {| ... |} in MediaWiki and rows can span several lines
+     *
+     * @param string $record
+     *
+     * @return string
+     */
+    private function convertTables($record) {
+        $pattern = "/\{\|([^\}]+)\|\}/m";
+        
+        preg_match_all($pattern, $record, $matches, PREG_OFFSET_CAPTURE);
+        
+        $tables = array();
+        foreach($matches[0] as $idx => $data)
+        {
+        	if (count($data) > 0)
+        	{
+        		$tables[] = array('start' => $data[1], 'length' => strlen($data[0]), 'table_content' => $data[0]);
+        	}
+        }
+        $reverse_indexes = array_reverse(array_keys($tables));
+        foreach($reverse_indexes as $idx)
+        {
+			$replaced = $this->processTableContent($tables[$idx]['table_content']);
+        	$record = substr_replace($record, $replaced, $tables[$idx]['start'], $tables[$idx]['length']);
+        }
+        
+		return $record;
+    }
+    /**
+     * The processing of tables is designed by trials and errors given the complexity of the tables description in
+     * wikimedia and the lack of structured description (grammar?) of the Wikimedia language.
+     * Don't expect wonders on complex / styled tables but it should work for simple tables
+     *
+     * @param string $record
+     *
+     * @return string
+     */
+    private function processTableContent($table)
+    {
+		$translit = array('|' => 'µ', '^' => '§'); // Replace some characters to make it easier to build regexpr !!!
+												   // since | and ^ have special meanings in PCRE and must be escaped
+		$table = str_replace(array_keys($translit), array_values($translit), $table);
+		
+		$table = str_replace("\n", '', $table);
+		$table = preg_replace(array("/\{µ([^µ!]+)/",  "/µ\}/"), '', $table);
+		$table = preg_replace(array("/µ-([^µ!]+)/"), 'µ-', $table);
+		$split_table = explode("µ-", $table);
+		$patterns = array(
+    			'/µ([^µ!]*)!/' => 'µ${1}@EXCLAM@TION', // Preserve ! inside table cells
+				'/µµ/'    => '|',
+    			'@ //$@' => '',
+    			'/^µ(.*)([^µ])$/' => '|${1}${2}|',
+				'/!!/' => '^',
+    			'/!/' => '^',
+    			'/µ/' => '|',
+    	);
+   		foreach($split_table as $l => $s)
+   		{
+   			if (trim($s) == '')
+   			{
+   				unset($split_table[$l]); // Remove empty lines	
+   			}
+   			else
+   			{
+   				$split_table[$l] = preg_replace(array_keys($patterns), array_values($patterns), $s);
+   				if (($split_table[$l][0] == '^') && ($split_table[$l][strlen($split_table[$l]) - 1] != '^'))
+   				{
+   					$split_table[$l] .= '^';
+   				}
+   			}
+   			
+   		}
+		$table = implode("\n", $split_table);
+		$table = str_replace("@EXCLAM@TION", '<nowiki>!</nowiki>', $table); // Restore ! inside table cells
+		
+   		return $table;
     }
 }
